b14469ee813481945b18f1ef7992a40e4a8c0753
Tenzin CAVAZZANA <tenzin.cavazzana@yrnet.com>
2025-09-08 16:22:58 +0200

    Modif3

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('operations/', views.operations_list, name='operations_list'),
+    path('operations/nouvelle/', views.operation_create, name='operation_create'),  # AVANT le pattern avec <int>
     path('operations/<int:operation_id>/', views.operation_detail, name='operation_detail'),
     path('operations/<int:operation_id>/duplicate/', views.operation_duplicate, name='operation_duplicate'),
     path('clients/', views.clients_list, name='clients_list'),
     path('clients/<int:client_id>/', views.client_detail, name='client_detail'),
-    path('operations/nouvelle/', views.operation_create, name='operation_create'),
 ]
