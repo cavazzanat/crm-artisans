522cb461a11dee598fa1b0b37f496e364883161f
Tenzin CAVAZZANA <tenzin.cavazzana@yrnet.com>
2025-09-19 16:10:56 +0200

    Modif2

--- core/models.py
+++ core/models.py
@@ -55,7 +55,7 @@ class Operation(models.Model):
     type_prestation = models.CharField(max_length=200)
     adresse_intervention = models.TextField()
     date_prevue = models.DateTimeField(null=True, blank=True)
-    date_intervention = models.DateTimeField(blank=True, null=True)
+    #date_intervention = models.DateTimeField(blank=True, null=True)
     statut = models.CharField(max_length=20, choices=STATUTS, default='en_attente_devis')
     date_creation = models.DateTimeField(auto_now_add=True)
     date_modification = models.DateTimeField(auto_now=True)
